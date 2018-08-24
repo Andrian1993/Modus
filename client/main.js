e13965409fd23bc36e3a7b4c3fb1cccdb187b758
Unknown <andriantsoy@gmail.com>
2018-08-25 00:15:09 +0900

    Start with Meteor.js

---
+++ client/main.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+import '../imports/ui/js/Navbar.js'
+import '../imports/ui/js/Footer.js'
+import '../imports/ui/js/Home.js'
+import '../imports/ui/js/Contacts.js'
+import '../imports/ui/js/Portfolio.js'
+
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function () {
+  this.render('Home');
+});
+
+Router.route('/Contacts', function () {
+  this.render('Contacts');
+});
+
+Router.route('/Portfolio', function () {
+  this.render('Portfolio');
+});
