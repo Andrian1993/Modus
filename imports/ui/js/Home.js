d08dae3f36969e2ddd3d660cefd16b4a7c874430
Unknown <andriantsoy@gmail.com>
2018-08-28 00:56:06 +0900

    Error fix

--- imports/ui/js/Home.js
+++ imports/ui/js/Home.js
@@ -1,6 +1,5 @@
 import "../Home.html"
 
-<<<<<<< HEAD
 if (Meteor.isClient) {
   Template.Home.onRendered(function(){
     $('nav ul li').hover(
@@ -13,29 +12,26 @@ if (Meteor.isClient) {
     );
   })
 }
-=======
 
 if (Meteor.isClient) {
     Template.Home.onRendered(function(){
       $('.site-color-purple').click(
           function () {
               $( 'nav, .footer' ).css( "background-color", "purple" );
-          }        
+          }
       );
 
       $('.site-color-aqua').click(
         function () {
             $( 'nav, .footer' ).css( "background-color", "#76c7c0" );
-        }        
+        }
     );
 
     $('.site-color-green').click(
         function () {
             $( 'nav, .footer' ).css( "background-color", "#70c570" );
-        }        
+        }
     );
 
     })
   }
-  
->>>>>>> dzhanik
