4311e3532fdc667a78c52f55279b7ffdf8e5e6d8
Unknown <andriantsoy@gmail.com>
2018-08-27 23:42:15 +0900

    slideDown in Portfolio navigation

--- imports/ui/js/Home.js
+++ imports/ui/js/Home.js
@@ -1,1 +1,14 @@
 import "../Home.html"
+
+if (Meteor.isClient) {
+  Template.Home.onRendered(function(){
+    $('nav ul li').hover(
+        function () {
+            $('ul', this).stop().slideDown(400);
+        },
+        function () {
+            $('ul', this).stop().slideUp(400);
+        }
+    );
+  })
+}
