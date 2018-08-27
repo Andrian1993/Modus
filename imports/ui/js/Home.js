efe3302b0e66862ead02a79dbb12fb344ba31566
frntnd93 <41643887+frntnd93@users.noreply.github.com>
2018-08-27 18:33:00 +0300

    site color change buttons

--- imports/ui/js/Home.js
+++ imports/ui/js/Home.js
@@ -1,1 +1,26 @@
 import "../Home.html"
+
+
+if (Meteor.isClient) {
+    Template.Home.onRendered(function(){
+      $('.site-color-purple').click(
+          function () {
+              $( 'nav, .footer' ).css( "background-color", "purple" );
+          }        
+      );
+
+      $('.site-color-aqua').click(
+        function () {
+            $( 'nav, .footer' ).css( "background-color", "#76c7c0" );
+        }        
+    );
+
+    $('.site-color-green').click(
+        function () {
+            $( 'nav, .footer' ).css( "background-color", "#70c570" );
+        }        
+    );
+
+    })
+  }
+  
