04ae25d908c0d0b147f8e2fb42885041d8de21fd
Unknown <andriantsoy@gmail.com>
2018-09-02 01:38:16 +0900

    add Sass and edited index.html

--- imports/ui/js/Footer.js
+++ imports/ui/js/Footer.js
@@ -1,1 +1,24 @@
 import "../Footer.html"
+
+if (Meteor.isClient) {
+    Template.Footer.onRendered(function(){
+      $('.site-color-purple').click(
+          function () {
+              $( 'nav, .footer' ).css( "background-color", "purple" );
+          }
+      );
+
+      $('.site-color-aqua').click(
+        function () {
+            $( 'nav, .footer' ).css( "background-color", "#76c7c0" );
+        }
+    );
+
+    $('.site-color-green').click(
+        function () {
+            $( 'nav, .footer' ).css( "background-color", "#70c570" );
+        }
+    );
+
+    })
+  }
--- imports/ui/js/Navbar.js
+++ imports/ui/js/Navbar.js
@@ -1,14 +1,14 @@
 import "../Navbar.html"
 
-if (Meteor.isClient) {
-  Template.Navbar.onRendered(function(){
-    $('nav ul li').hover(
-        function () {
-            $('ul', this).stop().slideDown(400);
-        },
-        function () {
-            $('ul', this).stop().slideUp(400);
-        }
-    );
-  })
-}
+// if (Meteor.isClient) {
+//   Template.Navbar.onRendered(function(){
+//     $('nav ul li').hover(
+//         function () {
+//             $('ul', this).stop().slideDown(400);
+//         },
+//         function () {
+//             $('ul', this).stop().slideUp(400);
+//         }
+//     );
+//   })
+// }
